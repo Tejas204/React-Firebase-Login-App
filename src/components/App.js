446a46ec775a837133f31a58561de1fe5e6bd439
Tejas204 <tejasdhopavkar2000@gmail.com>
2023-06-06 22:38:43 +0530

    Added sign up component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,14 @@
+import Signup from "./Signup";
+import { Container } from "react-bootstrap";
+
 function App() {
   return (
-    "Hello World"
+    <Container className="d-flex align-items-center justify-items-center justify-content-center" style={{minHeight:"100vh"}}>
+      <div className="w-100" style={{maxWidth: '400px'}}>
+        <Signup></Signup>
+      </div>
+    </Container>
+    
   );
 }
 
